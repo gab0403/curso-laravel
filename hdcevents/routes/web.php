5db246f6b69ba929f55fd3801d42fe8e130f9146
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-23 14:15:57 -0300

    feat: adiciona imagens, header e footer e arruma navbar

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [10,20,30,40,50];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome',
+        [
+            'nome' => $nome,
+            'idade2' => $idade,
+            'profissao' => "Programador",
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+
+    $busca = request('search');
+
+    return view('products', ['busca' => $busca]);
+});
+
+Route::get('/produtos_teste/{id?}', function ($id = null) {
+    return view('product', ['id' => $id]);
 });
