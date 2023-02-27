fe5a7f6b55d13032b42ea146796d96cc388a8cc9
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-27 14:04:55 -0300

    feat: exibindo dados do usuario na view

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
+
 
 class EventController extends Controller {
 
@@ -53,7 +55,7 @@ class EventController extends Controller {
         }
 
         $user = auth()->user();
-        $event->user_id = $user->$id;
+        $event->user_id = $user->id;
 
         $event->save();
 
@@ -63,6 +65,9 @@ class EventController extends Controller {
     public function show($id) {
 
         $event = Event::findOrFail($id);
-        return view('events.show', ['event' => $event]);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
