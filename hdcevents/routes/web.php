a6d8af6c218e768ca90bdc2819d2a4f879474101
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-27 13:39:44 -0300

    feat: autenticação login e registro com jetstream

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -26,3 +26,7 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
