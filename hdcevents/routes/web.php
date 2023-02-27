da4a30e6065f71ba1c89b5dd59fe01bfd2432a42
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-27 14:40:17 -0300

    feat: deletando dados

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', [EventController::class, 'index'] );
 Route::get('/events/create', [EventController::class, 'create'] )->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show'] );
 Route::post('/events', [EventController::class, 'store'] );
+Route::delete('/events/{id}', [EventController::class, 'destroy'] );
 
 
 
@@ -27,5 +28,7 @@ Route::get('/contact', function () {
 });
 
 Route::get('/dashboard', [EventController::class, 'dashboard'] )->middleware('auth');
+
+
 
 
