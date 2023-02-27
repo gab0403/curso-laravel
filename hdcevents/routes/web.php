e3ac9bcb971308084d077e58bb653964b05b4ea3
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2023-02-27 15:51:10 -0300

    feat: removendo presença no evento

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -32,4 +32,7 @@ Route::get('/dashboard', [EventController::class, 'dashboard'] )->middleware('au
 
 Route::post('/events/join/{id}', [EventController::class, 'joinEvent'] )->middleware('auth');
 
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'] )->middleware('auth');
+
+
 
